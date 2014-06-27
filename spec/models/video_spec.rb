0320345f24ba6d352961cdb4009000e4402def12
Haisong Huang <Haisong@HaisongdeMacBook-Pro.local>
2014-06-27 16:09:54 +0800

    test

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,9 +1,6 @@
-require 'spec_helper'
-
+require 'rails_helper'
 describe Video do 
-  it "save itself" do
-    video = Video.new(title: "monk", description: "a great video")
-    video.save
-    expect(Video.first).to eq(video)
-  end
+  it { should belong_to(:category)}
+  it { should validate_presence_of(:title)}
+  it { should validate_presence_of(:description)}
 end
