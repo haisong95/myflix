2cebb018fdcf781c6d89f0634abd805f1539fbee
Haisong Huang <Haisong@x-134-84-147-201.uofm-secure.wireless.umn.edu>
2014-10-16 02:08:27 -0500

    new start

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+      comedies = Category.create(name: "comedies")
+      dramas = Category.create(name: "dramas")
+      futurama = Video.create(title: "Futurama", description: "space travel", category: comedies, small_cover_url: "tmp/family_guy.jpg", large_cover_url: '/tmp/monk_large.jpg')
+      futurama = Video.create(title: "Futurama", description: "space travel", category: comedies, small_cover_url: "tmp/family_guy.jpg", large_cover_url: '/tmp/monk_large.jpg')
+      futurama = Video.create(title: "Futurama", description: "space travel", category: comedies, small_cover_url: "tmp/family_guy.jpg", large_cover_url: '/tmp/monk_large.jpg')
+      futurama = Video.create(title: "Futurama", description: "space travel", category: comedies, small_cover_url: "tmp/family_guy.jpg", large_cover_url: '/tmp/monk_large.jpg')
+      futurama = Video.create(title: "Futurama", description: "space travel", category: comedies, small_cover_url: "tmp/family_guy.jpg", large_cover_url: '/tmp/monk_large.jpg')
+      futurama = Video.create(title: "Futurama", description: "space travel", category: comedies, small_cover_url: "tmp/family_guy.jpg", large_cover_url: '/tmp/monk_large.jpg')
+      futurama = Video.create(title: "Futurama", description: "space travel", category: comedies, small_cover_url: "tmp/family_guy.jpg", large_cover_url: '/tmp/monk_large.jpg')
+      futurama = Video.create(title: "Futurama", description: "space travel", category: comedies, small_cover_url: "tmp/family_guy.jpg", large_cover_url: '/tmp/monk_large.jpg')
+      
+      futurama = Video.create(title: "Futurama", description: "space travel", category: dramas, small_cover_url: "tmp/family_guy.jpg", large_cover_url: '/tmp/monk_large.jpg')
+      futurama = Video.create(title: "Futurama", description: "space travel", category: dramas, small_cover_url: "tmp/family_guy.jpg", large_cover_url: '/tmp/monk_large.jpg')
+      futurama = Video.create(title: "Futurama", description: "space travel", category: dramas, small_cover_url: "tmp/family_guy.jpg", large_cover_url: '/tmp/monk_large.jpg')
+      futurama = Video.create(title: "Futurama", description: "space travel", category: dramas, small_cover_url: "tmp/family_guy.jpg", large_cover_url: '/tmp/monk_large.jpg')
+      futurama = Video.create(title: "Futurama", description: "space travel", category: dramas, small_cover_url: "tmp/family_guy.jpg", large_cover_url: '/tmp/monk_large.jpg')
+      futurama = Video.create(title: "Futurama", description: "space travel", category: dramas, small_cover_url: "tmp/family_guy.jpg", large_cover_url: '/tmp/monk_large.jpg')
+      futurama = Video.create(title: "Futurama", description: "space travel", category: dramas, small_cover_url: "tmp/family_guy.jpg", large_cover_url: '/tmp/monk_large.jpg')
+      futurama = Video.create(title: "Futurama", description: "space travel", category: dramas, small_cover_url: "tmp/family_guy.jpg", large_cover_url: '/tmp/monk_large.jpg')
+      futurama = Video.create(title: "Futurama", description: "space travel", category: dramas, small_cover_url: "tmp/family_guy.jpg", large_cover_url: '/tmp/monk_large.jpg')
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 describe Category do
-it { should have_many(:videos)}
+  it { should have_many(:videos)}
+
+  describe "#recent_videos" do
+    it "returns the videos in the reverse chronical order by created_at" do
+      comedies = Category.create(name: comedies)
+      futurama = Video.create(title: "Futurama", description: "space travel", category: comedies, created_at: 1.day.ago)
+      south_park = Video.create(title: "South Park", description: "FUnny kids", category: comedies)
+      expect(comedies.recent_videos).to eq([south_park,futurama])
+    end
+    it "returns all the videos if there are less than 6 videos" do
+      comedies = Category.create(name: comedies)
+      futurama = Video.create(title: "Futurama", description: "space travel", category: comedies, created_at: 1.day.ago)
+      south_park = Video.create(title: "South Park", description: "FUnny kids", category: comedies)
+      expect(comedies.recent_videos.count).to eq(2)
+    end      
+    it "returns 6 videos if there are more than 6 videos" do
+      comedies = Category.create(name: comedies)
+      i = 0
+      k = 0
+      7.times { Video.create(title: "foo"+ "#{i+=1}", description: "#{k+=1}",category: comedies)}
+      expect(comedies.recent_videos.count).to eq(6)
+    end
+    it "returns most recent 6 vidoes " do
+      comedies = Category.create(name: comedies)
+      6.times { Video.create(title: "foo", description: "bar",category: comedies)}
+      tonighta_show = Video.create(title: "tonights show", description: "beautiful", category: comedies, created_at: 1.day.ago)
+      expect(comedies.recent_videos).not_to include(tonighta_show)
+    end
+
+    it "returns empty array if no videos in category" do
+       comedies = Category.create(name: comedies)
+       expect(comedies.recent_videos).to eq([])
+    end
+
+  end
 end
