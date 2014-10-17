4959714431e23d48bcfbc84b6dc59b33a861063c
Haisong Huang <Haisong@x-160-94-137-151.uofm-secure.wireless.umn.edu>
2014-10-16 22:15:24 -0500

    cant save user error

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Myflix::Application.routes.draw do
-  root 'videos#index'
-  get 'ui(/:action)', controller: 'ui'
-  get '/home', to: "videos#index"
-
+  get "user/new"
+  get "pages/front"
+  root to: "pages#front"
   resources :videos, only: [:show] do
     collection do
       post :search, to: "videos#search"
     end
   end
-  resources :categories, only: [:show]
-
+  get 'ui(/:action)', controller: 'ui'
+  get 'register', to: "users#new"
+  resources :users, only: [:create]
 end
